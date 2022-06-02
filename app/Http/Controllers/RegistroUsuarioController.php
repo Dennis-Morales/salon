17d287f2ac01c4a0edc277af1c022ffac3922d37
Rubén Espinosa Chamé <98977457+RubenE10@users.noreply.github.com>
2022-06-02 11:13:13 -0500

    Agregue funciones en el controlador

--- app/Http/Controllers/RegistroUsuarioController.php
+++ app/Http/Controllers/RegistroUsuarioController.php
@@ -7,5 +7,54 @@ use Illuminate\Http\Request;
 
 class RegistroUsuarioController extends Controller
 {
-    
+     public function create(){
+        return view('registrar-usuario');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'nombre'=>'required',
+            'apellido_p'=>'required',
+            'apellido_m'=>'required',
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+
+        $user = new User();
+        $user->nombre = $request->nombre;
+        $user->apellido_p = $request->apellido_p;
+        $user->apellido_m = $request->apellido_m;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->rol = "Cliente";
+
+        $user->save();
+        return redirect()->route('registers.create');
+    }
+
+    public function registrar(Request $request){
+        $request->validate([
+            'nombre'=>'required',
+            'apellido_p'=>'required',
+            'apellido_m'=>'required',
+            'email'=>'required',
+            'password'=>'required',
+            'rol'=>'required'
+        ]);
+
+        $user = new User();
+        $user->nombre = $request->nombre;
+        $user->apellido_p = $request->apellido_p;
+        $user->apellido_m = $request->apellido_m;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->rol = $request->rol;
+
+        $user->save();
+        return redirect()->route('users.gerente');
+    }
+
+    public function view(){
+        return view('registrar-user');
+    }
 }
