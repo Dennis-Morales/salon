7db754a738e42fa192ab2090ffb2e57b83ae04ed
Rubén Espinosa Chamé <98977457+RubenE10@users.noreply.github.com>
2022-06-02 11:19:36 -0500

    Agregue algunas rutas

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,9 @@ Route::get('registers', [RegistroUsuarioController::class, 'create'])->name('reg
 Route::post('registers', [RegistroUsuarioController::class, 'store'])->name('registers.store');
 Route::get('users/cliente', [UserController::class, 'cliente'])->name('users.cliente');
 Route::get('users', [UserController::class, 'create'])->name('users.create');
+Route::get('users-anonimo', [UserController::class, 'anonimo'])->name('users.anonimo');
+Route::get('eventos-viewcreate', [EventoController::class, 'viewcreate'])->name('eventos.viewcreate');
+Route::post('eventos-create/{paquetes}', [EventoController::class, 'create'])->name('eventos.create');
+Route::get('users/gerente', [UserController::class, 'gerente'])->name('users.gerente');
+Route::get('registers/view', [RegistroUsuarioController::class, 'view'])->name('registers.view');
+Route::get('paquetes-create', [PaqueteController::class, 'create'])->name('paquetes.create');
